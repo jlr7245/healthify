fff56bbfe1c25a147785b15a47570a2ceb4fcd22
J Silverstein <jess.pebble@gmail.com>
2017-02-03 09:13:06 -0500

    setting up app.js

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const session = require('express-session');
+const passport = require('passport');
 
-var app = express();
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
 
 require('dotenv').config();
 
