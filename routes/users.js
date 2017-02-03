a1378d12fdfa1bdfe5e3605705783702c1da8329
Tenzin Tsagong <tsagong@gmail.com>
2017-02-03 14:54:42 -0500

    user-auth

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var authHelpers = require('../auth/auth-helpers');
+
 
 /* GET users listing. */
+
+router.get('/', authHelpers.loginRequired, (req,res,next) => {
+  res.render('user/index', {
+    user: req.user.dataValues,
+    title: 'user',
+    currentRoute: 'user'
+  });
+});
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
 
 module.exports = router;
