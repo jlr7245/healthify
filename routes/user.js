6ade1af900876ff080b91b038d98f12d22bb4e24
J Silverstein <jess.pebble@gmail.com>
2017-02-04 12:19:25 -0500

    api call UNLOCKED!

--- routes/user.js
+++ routes/user.js
@@ -8,9 +8,10 @@ const dashRenders = require('../dash/dash-render');
 
 /* GET users listing. */
 
-router.get('/', authHelpers.loginRequired, (req,res,next) => {
+router.get('/', authHelpers.loginRequired, dashRenders.renderFoods, (req,res,next) => {
   res.render('user/index', {
     user: req.user.dataValues,
+    foods: res.locals.foods,
     title: 'user',
     currentRoute: 'user'
   });
