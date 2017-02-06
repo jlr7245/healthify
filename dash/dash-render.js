788d1a9469b8fc342bc3a70b6ac1de89069ab610
J Silverstein <jess.pebble@gmail.com>
2017-02-06 10:25:07 -0500

    working on meal rendering

--- dash/dash-render.js
+++ dash/dash-render.js
@@ -41,6 +41,7 @@ function postFoodsIntoDatabase(req,res,next) {
       carbohydrates: food.nf_total_carbohydrate,
       totalFat: food.nf_total_fat,
       sodium: food.nf_sodium,
+      fiber: food.nf_dietary_fiber,
       imageURL: food.photo.highres,
       belongsTo: req.user.dataValues.id,
       updated: false
@@ -49,8 +50,40 @@ function postFoodsIntoDatabase(req,res,next) {
   return next();
 }
 
+function postMeal(req,res,next) {
+  let totalCalories = 0;
+  let totalProtein = 0;
+  let totalCarbs = 0;
+  let totalFiber = 0;
+  let totalFat = 0;
+  let totalSodium = 0;
+  res.locals.foodData.forEach((food) => {
+    totalCalories += parseInt(food.nf_calories);
+    totalProtein += parseInt(food.nf_protein);
+    totalCarbs += parseInt(food.nf_total_carbohydrate);
+    totalFat += parseInt(food.nf_total_fat);
+    totalSodium += parseInt(food.nf_sodium);
+    totalFiber += parseInt(food.nf_dietary_fiber);
+  });
+  console.log(totalCalories, totalProtein, totalCarbs);
+  models.UserMeals.create({
+    ingredients: req.body.foodName,
+    comments: req.body.comments,
+    whichMeal: req.body.whichmeal,
+    belongsTo: req.params.id,
+    totalCalories: totalCalories,
+    totalProtein: totalProtein,
+    totalCarbs: totalCarbs,
+    totalFiber: totalFiber,
+    totalSodium: totalSodium,
+    totalFat: totalFat
+  });
+  return next();
+}
+
 module.exports = {
   renderFoods,
   getFoodInfo,
-  postFoodsIntoDatabase
+  postFoodsIntoDatabase,
+  postMeal
 };
