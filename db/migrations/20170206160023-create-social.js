cd664bcc15890a1f4803e23fba2bd10d67a4a8b2
Tenzin Tsagong <tsagong@gmail.com>
2017-02-06 11:52:26 -0500

    changes

---
+++ db/migrations/20170206160023-create-social.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Socials', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING(128),
+        unique: true
+      },
+      isNew: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Socials');
+  }
+};
---
+++ db/models/social.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Social = sequelize.define('Social', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING(128),
+    unique: true,
+    isNew: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Social;
+};
